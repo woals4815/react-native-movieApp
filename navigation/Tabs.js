3a5c88c0fce0ff1807f9184fc817e2754cb81ecc
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-02-13 20:03:48 +0900

    Add Ionicon in TabBar

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Fav from "../screens/Fav";
 import { useLayoutEffect } from "react";
+import { Platform } from "react-native";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
 const Tabs = createBottomTabNavigator();
 
-const getHeaderName = (route) =>
-  route?.state?.routeNames[route.state.index] || "Movies";
+const getHeaderName = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Movies";
+  return routeName; //route?.state?.routeNames[route.state.index] || "Movies"; (temporary solution)
+};
 export default (props) => {
   const { route, navigation } = props;
   useLayoutEffect(() => {
@@ -19,11 +24,40 @@ export default (props) => {
     });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Movies") {
+            iconName += "film";
+          } else if (route.name === "TV") {
+            iconName += "tv";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favourites") {
+            iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? "white" : "grey"}
+              size={26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
-      <Tabs.Screen name="Favs" component={Fav} />
+      <Tabs.Screen name="Favourites" component={Fav} />
     </Tabs.Navigator>
   );
 };
